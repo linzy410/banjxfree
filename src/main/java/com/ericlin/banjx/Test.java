5453c34cdd66423bde7c1ff62d66bcdf63e4e294
linzheyan <linzheyan@qnvip.com>
2016-03-03 10:03:50 +0800

    date test

---
+++ src/main/java/com/ericlin/banjx/Test.java
@@ -0,0 +1,8 @@
+package com.ericlin.banjx;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date()));
+	}
+}
