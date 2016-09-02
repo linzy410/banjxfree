6b559eb01896cb6dea6e910c22dc45b66f713c36
linzheyan <linzheyan@qnvip.com>
2016-09-02 11:43:18 +0800

    fix timer

--- src/main/java/com/ericlin/mock/taobao/paimai/timer/JianLouTask.java
+++ src/main/java/com/ericlin/mock/taobao/paimai/timer/JianLouTask.java
@@ -1,27 +1,33 @@
 package com.ericlin.mock.taobao.paimai.timer;
 
+import java.util.Calendar;
+
 import com.ericlin.mock.taobao.paimai.Jianlou;
 import com.qnvip.util.DateFormatUtil;
 import com.qnvip.util.scheduling.Scheduler;
 import com.qnvip.util.scheduling.SchedulerTask;
-import com.qnvip.util.scheduling.iterators.HourIterator;
+import com.qnvip.util.scheduling.iterators.TimeIterator;
 
 public class JianLouTask {
 
 	private Scheduler scheduler = new Scheduler();
 	
 	public void start() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.MINUTE, 55);
+		String startTime = DateFormatUtil.formatDateTime(cal.getTime());
 		scheduler.schedule(new SchedulerTask() {
 			@Override
 			public void run() {
 				try {
 					Jianlou.fetch();
 				} catch (Exception e) {
+					e.printStackTrace();
 				}
 				System.out.println(DateFormatUtil.getCurrentTime() + " OK");
 			}
-
-		}, new HourIterator(0, 55, 0));
+			
+		}, new TimeIterator(startTime, 60));
 	}
 	
 	public static void main(String[] args) {
